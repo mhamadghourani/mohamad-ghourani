d2f0633edf936db3cd6019445eb6b46d5f9556b3
mhamadghourani <mhamadghourani155@gmail.com>
2024-12-12 20:40:19 +0200

    fixing issue #12

--- src/project.js
+++ src/project.js
@@ -3,12 +3,12 @@ export const project = [
       image: "/mohamad-ghourani/projectimg.png",  // Image path relative to public
       header: "This Portfolio website",
       paragraph: "Create this portfolio website using ReactJS and enhance its interactivity with Framer Motion for smooth animations and dynamic effects.",
-      link:"/",
+      link:"https://github.com/mhamadghourani/mohamad-ghourani",
     },
     {
         image:"/mohamad-ghourani/figmaproject.png",
         header:"UI/UX project",
         paragraph:"A Figma UI/UX project involves designing intuitive interfaces and seamless user experiences.",
-        link:"/",
+        link:"https://github.com/mhamadghourani/mohamad-ghourani",
     },
   ];
